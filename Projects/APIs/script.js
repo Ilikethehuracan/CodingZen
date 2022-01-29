301926a7ce7f376f1322146b4375cf8576b0a7de
Krish Gupta <krish@Krishs-MacBook-Pro.local>
2022-01-29 15:45:17 -0600

    1/29/22

--- Projects/APIs/script.js
+++ Projects/APIs/script.js
@@ -1,12 +1,28 @@
-var api_key = "6335ab3930970f405e5bed398895a08c"
+var api_key = "1fd967811a0e544378a8d835814e9846"
 function check() {
     var cityname = document.getElementById("inp").value
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=6335ab3930970f405e5bed398895a08c`).then(gotResponse).then(gotFinalResponse)
+    document.querySelector("#inp").value = ""
+    document.querySelector(".temp").innerHTML = "Loading..."
+    document.querySelector(".low").innerHTML = "Loading..."
+    document.querySelector(".high").innerHTML = "Loading..."
+    document.querySelector(".feelslike").innerHTML = "Loading..."
+    document.querySelector(".windspeed").innerHTML = "Loading..."
+    document.querySelector(".humidity").innerHTML = "Loading..."
+    document.querySelector("body").style.cursor = "progress"
+    document.querySelector("button").style.cursor = "progress"
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=1fd967811a0e544378a8d835814e9846`).then(gotResponse).then(gotFinalResponse)
 }
 function gotResponse(response) {
     return response.json()
 }
 function gotFinalResponse(finalResponse) {
     console.log(finalResponse)
-    alert(finalResponse.main.temp_min + " Is the minimum temperature in Kelvin!")
+    document.querySelector("body").style.cursor = "default"
+    document.querySelector("button").style.cursor = "default"
+    document.querySelector(".temp").innerHTML = finalResponse.main.temp + " is the temperature for today (in Kelvin)!"
+    document.querySelector(".low").innerHTML = finalResponse.main.temp_min + " is the low for today (in Kelvin)!"
+    document.querySelector(".high").innerHTML = finalResponse.main.temp_max + " is the high for today (in Kelvin)!"
+    document.querySelector(".feelslike").innerHTML = finalResponse.main.feels_like + " is how it feels like today (in Kelvin)!"
+    document.querySelector(".windspeed").innerHTML = finalResponse.wind.speed + " is the wind speed for today!"
+    document.querySelector(".humidity").innerHTML = finalResponse.main.humidity + "% is the humidity for today!"
 }
